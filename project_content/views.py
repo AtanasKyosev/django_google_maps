a0051c090eacc1c558e64030b9ee636477fdd621
AtanasKyosev <159782687+AtanasKyosev@users.noreply.github.com>
2024-06-21 14:13:25 +0300

    Creating model, url, view, template and simple css to display locations

--- project_content/views.py
+++ project_content/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from project_content.models import Locations
+
+
+class HomeView(ListView):
+    template_name = "project_content/home.html"
+    context_object_name = 'mydata'
+    model = Locations
+    success_url = "/"
